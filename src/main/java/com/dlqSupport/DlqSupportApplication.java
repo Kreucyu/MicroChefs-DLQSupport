5f3a8482217462bb7fc93cda2b50f1d227ea6d71
Kreucyu <elciocleiton72@gmail.com>
2026-05-01 16:57:07 -0300

    build: Serviço de manutenção dos dados recebidos na DLQ para correção manual

---
+++ src/main/java/com/dlqSupport/DlqSupportApplication.java
@@ -0,0 +1,13 @@
+package com.dlqSupport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlqSupportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlqSupportApplication.class, args);
+	}
+
+}
