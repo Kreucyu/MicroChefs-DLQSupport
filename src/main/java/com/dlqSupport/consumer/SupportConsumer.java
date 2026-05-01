687ef6d52ca4e82bc68470935b7c72ab29eb73cf
Kreucyu <elciocleiton72@gmail.com>
2026-05-01 20:20:14 -0300

    build: iniciando html que irá ter a interface de editor usando Moanco Editor

--- src/main/java/com/dlqSupport/consumer/SupportConsumer.java
+++ src/main/java/com/dlqSupport/consumer/SupportConsumer.java
@@ -1,4 +1,23 @@
 package com.dlqSupport.consumer;
 
+import com.dlqSupport.service.SupportService;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+import tools.jackson.databind.ObjectMapper;
+
+@Component
 public class SupportConsumer {
+
+    @Autowired
+    private SupportService supportService;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @RabbitListener(queues = { "dead-letter-queue" })
+    public void receberMensagem(@Payload String message) {
+        System.out.println(message);
+    }
+
 }
