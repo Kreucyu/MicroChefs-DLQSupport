fca2444f60ba3288cec53b3695f32940a85b97d0
Kreucyu <elciocleiton72@gmail.com>
2026-05-04 20:28:30 -0300

    chore: melhoria na UI do editor e utilização do @JSONInclude para remover valores nulos na exibição da estrutura desejada

--- src/main/java/com/dlqSupport/entities/CamposExigidos.java
+++ src/main/java/com/dlqSupport/entities/CamposExigidos.java
@@ -1,8 +1,11 @@
 package com.dlqSupport.entities;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.List;
 import java.util.Map;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record CamposExigidos(
         String tipo,
         Boolean isObrigatorio,
