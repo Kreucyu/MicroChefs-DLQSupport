18f4f0376b965f84b54b8d804c03bdeadb6ac67b
Kreucyu <elciocleiton72@gmail.com>
2026-05-04 13:58:54 -0300

    chore: adaptando producer do DLQ

--- src/main/java/com/dlqSupport/producer/MensagemProducer.java
+++ src/main/java/com/dlqSupport/producer/MensagemProducer.java
@@ -1,5 +1,6 @@
 package com.dlqSupport.producer;
 
+import com.dlqSupport.dto.RecoveryMessageDTO;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -12,11 +13,11 @@ public class MensagemProducer {
     @Autowired
     private AmqpTemplate amqpTemplate;
 
-    public void reenviarJson(String json) {
+    public void reenviarJson(RecoveryMessageDTO recoveryMessageDTO) {
         amqpTemplate.convertAndSend(
                 "pedido-exchange",
                 "pedido-key.pago",
-                objectMapper.writeValueAsString(json)
+                objectMapper.writeValueAsString(recoveryMessageDTO.mensagemCorrigida())
         );
     }
 
